6e9752193dcd6d8e24af6381cd5505fc6d9dc8c0
Zemian Deng <zemian.deng@oracle.com>
2020-02-16 00:55:55 -0500

    Fix svg to display example

--- src/js/examples/svg.js
+++ src/js/examples/svg.js
@@ -1,27 +1,9 @@
 /*
  */
 define(['knockout',
-        'examples/svg-ko-template-loader',
         'ojs/ojcomposite',
         'ojs/ojknockout'],
-    function (ko, SvgTemplateLoader, Composite) {
-
-        // Push template loader to the front of the loader list
-        ko.components.loaders.unshift(new SvgTemplateLoader());
-
-        function valueToPoint(value, index, total) {
-            var x     = 0;
-            var y     = -value * 0.8;
-            var angle = Math.PI * 2 / total * index;
-            var cos   = Math.cos(angle);
-            var sin   = Math.sin(angle);
-            var tx    = x * cos - y * sin + 100;
-            var ty    = x * sin + y * cos + 100;
-            return {
-                x: tx,
-                y: ty
-            };
-        }
+    function (ko, Composite) {
 
         // function SvgAxisLabelVM(context) {
         //     this.stat =context.properties.stat;
@@ -37,19 +19,6 @@ define(['knockout',
         //     }, this);
         // }
 
-        function SvgPolygraphVM(context) {
-            console.log(context);
-            // a computed property for the polygon's points
-            // this.points = ko.computed(function () {
-            //     var stats = context.properties.stats;
-            //     var total = stats.length;
-            //     return stats.map(function (stat, i) {
-            //         var point = valueToPoint(stat.value, i, total);
-            //         return point.x + ',' + point.y
-            //     }).join(' ');
-            // }, this);
-        }
-
         function ViewModel() {
             this.stats = [
                 { label: 'A', value: 100 },
@@ -59,53 +28,52 @@ define(['knockout',
                 { label: 'E', value: 100 },
                 { label: 'F', value: 100 }
             ];
-            // this.statsObsArray = ko.observableArray(this.stats);
-            // this.newLabel = ko.observable('');
-            // this.statsJsonText = ko.computed(()=> {
-            //     return JSON.stringify(this.statsObsArray(), null, 2);
-            // });
-            //
-            // this.add = (e) => {
-            //     e.preventDefault();
-            //     if (!this.newLabel) return;
-            //     this.stats.push({
-            //         label: this.newLabel,
-            //         value: 100
-            //     });
-            //     this.newLabel = '';
-            // };
-            //
-            // this.remove = (stat) => {
-            //     if (this.stats.length > 3) {
-            //         this.stats.splice(this.stats.indexOf(stat), 1);
-            //     } else {
-            //         alert('Can\'t delete more!');
-            //     }
-            // };
 
-            // metadata: {
-            //     properties: {
-            //         stats: { type: "array" }
-            //     }
-            // }
+            this.valueToPoint = function(value, index, total) {
+                var x     = 0;
+                var y     = -value * 0.8;
+                var angle = Math.PI * 2 / total * index;
+                var cos   = Math.cos(angle);
+                var sin   = Math.sin(angle);
+                var tx    = x * cos - y * sin + 100;
+                var ty    = x * sin + y * cos + 100;
+                return {
+                    x: tx,
+                    y: ty
+                };
+            };
 
-            this.connected = () => {
-                // This is not working!
-                // TODO: What's the proper way to use KO template loader?
-                Composite.register("svg-polygraph", {
-                    viewModel: SvgPolygraphVM,
-                    view: { svgTemplate: 'svg-polygraph-template' },
+            this.points = ko.computed(function() {
+                var total = this.stats.length;
+                var valueToPoint = this.valueToPoint;
+                return this.stats.map(function (stat, i) {
+                    var point = valueToPoint(stat.value, i, total);
+                    return point.x + ',' + point.y
+                }).join(' ');
+            }, this);
+
+            this.statsObsArray = ko.observableArray(this.stats);
+            this.newLabel = ko.observable('');
+            this.statsJsonText = ko.computed(()=> {
+                return JSON.stringify(this.statsObsArray(), null, 2);
+            });
+
+            this.add = (e) => {
+                e.preventDefault();
+                if (!this.newLabel) return;
+                this.stats.push({
+                    label: this.newLabel,
+                    value: 100
                 });
+                this.newLabel = '';
+            };
 
-                // Composite.register("svg-axis-label", {
-                //     viewModel: SvgAxisLabelVM,
-                //     view: document.getElementById("svg-axis-label-template").innerHTML,
-                //     metadata: {
-                //         properties: {
-                //             stats: { type: "array" }
-                //         }
-                //     }
-                // });
+            this.remove = (stat) => {
+                if (this.stats.length > 3) {
+                    this.stats.splice(this.stats.indexOf(stat), 1);
+                } else {
+                    alert('Can\'t delete more!');
+                }
             };
         }
 

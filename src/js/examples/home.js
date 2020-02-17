742cb97d5899e0f5574578722ca34852260199db
Zemian Deng <zemian.deng@oracle.com>
2020-02-17 11:58:41 -0500

    Add serverTime to home page

--- src/js/examples/home.js
+++ src/js/examples/home.js
@@ -1,6 +1,15 @@
-define([],
-    function () {
+define(['knockout'],
+    function (ko) {
         function ExampleViewModel() {
+            this.serverTime = ko.observable(new Date());
+
+            this.connected = () => {
+                let updateServerTime = () => {
+                    this.serverTime(new Date());
+                    setTimeout(updateServerTime, 1000);
+                };
+                updateServerTime();
+            };
         }
         return new ExampleViewModel();
     }

a7beb868a8780caa002401394a177d1d088b61c3
Zemian Deng <zemian.deng@oracle.com>
2020-03-19 01:24:57 -0400

    Add modal-oj-dialog example

---
+++ src/js/examples/modal-oj-dialog.js
@@ -0,0 +1,17 @@
+define(['knockout',
+    'ojs/ojbutton',
+    'ojs/ojdialog',
+    'ojs/ojknockout'
+], function (ko) {
+    function ExampleViewModal() {
+        this.onOpenModal = (event) => {
+            document.getElementById('demo-modal-oj-dialog-1').open();
+        };
+
+        this.onCloseModal = (event) => {
+            document.getElementById('demo-modal-oj-dialog-1').close();
+        };
+    }
+
+    return ExampleViewModal;
+});
